1211661b8eb22d3790278be68f810f30cfa02d13
FrozenCrash <zzcrash@gmail.com>
2017-11-06 17:34:35 +0200

    Add user test for downcase email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,4 +101,13 @@ describe User do
       specify { expect(user_for_invalid_password).to be false }
     end
   end
+
+  describe "user email downcase" do 
+    let(:mixed_case_email) { "Foo@ExAmPlE.CoM" }
+    it "should be saved as all lower case" do 
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 end
